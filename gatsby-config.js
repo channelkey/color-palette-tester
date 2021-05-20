cc99b17af0af1c3e32549e18ea8590f402042063
channelkey <149203@gmail.com>
2021-05-20 14:01:13 -0400

    Bootstrap 5 installed, working

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
-  siteMetadata: {
-    title: "Color Palette Tester",
-  },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+   siteMetadata: {
+      title: "Color Palette Tester",
+   },
+   plugins: [
+      "gatsby-plugin-gatsby-cloud",
+      {
+         resolve: `gatsby-plugin-sass`,
+         options: {
+            implementation: require("sass"),
+            postCssPlugins: [],
+            sassOptions: {
+               precision: 10,
+            },
+         },
+      },
+   ],
 };
